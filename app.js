2f69e44bcb8eab7c5e23bce6a71a769d799d9925
Fagbemi Oluwaseyi <seyifagbemi211@gmail.com>
2022-01-26 11:51:49 +0100

    completed website

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-    
+const hamburger = document.getElementById("hamburger");
+const headerUL = document.getElementById("header-ul");
+const signupUL = document.getElementById("sign-up");
+
+hamburger.addEventListener("click", () => {
+  headerUL.classList.toggle("show");
+  signupUL.classList.toggle("show");
+});
